beec5889b9ca07f7050d9f8050c962380cf6b39c
CharlyNd <charles.etienne.ndiaye@gmail.com>
2025-05-27 14:06:10 +0200

    feat: create operationSummary component

--- components/operation-list/OperationList.tsx
+++ components/operation-list/OperationList.tsx
@@ -1,6 +1,7 @@
 import { View } from 'react-native';
 import { useState } from 'react';
 import { OperationSearch } from './OperationSearch';
+import { OperationSummary } from './OperationSummary';
 
 export function OperationList() {
   const [search, setSearch] = useState('');
@@ -8,6 +9,7 @@ export function OperationList() {
   return (
     <View className="flex-1 bg-white">
       <OperationSearch value={search} onChange={setSearch} />
+      <OperationSummary credit={1000.35} debit={220} balance={500} />
     </View>
   );
 }
---
+++ components/operation-list/OperationSummary.tsx
@@ -0,0 +1,45 @@
+import { View, Text } from 'react-native';
+import Colors from '~/constants/Colors';
+
+interface OperationSummaryProps {
+  credit: number;
+  debit: number;
+  balance: number;
+}
+
+export function OperationSummary({ credit, debit, balance }: OperationSummaryProps) {
+  return (
+    <View className="flex-row justify-between bg-white rounded-2xl px-5 py-4 m-6" style={{ boxShadow: '4px 6px 7px 5px rgba(0, 0, 0, 0.07)' }}>
+      <View className="items-center">
+        <View className='items-end'>
+          <Text className="text-xl font-semibold font-[Inter]" style={{ color: Colors.black.text }}>
+            {credit % 1 === 0
+              ? credit.toLocaleString('fr-FR', { style: 'currency', currency: 'EUR', minimumFractionDigits: 0, maximumFractionDigits: 0 })
+              : credit.toLocaleString('fr-FR', { style: 'currency', currency: 'EUR' })}
+          </Text>
+          <Text className="text-gray-600 text-base mt-1" style={{ color: Colors.labelSummary }}>Crédit</Text>
+        </View>
+      </View>
+      <View className="items-center">
+        <View className='items-end'>
+          <Text className="text-lg font-semibold font-[Inter]" style={{ color: Colors.red.text }}>
+            {debit % 1 === 0
+              ? debit.toLocaleString('fr-FR', { style: 'currency', currency: 'EUR', minimumFractionDigits: 0, maximumFractionDigits: 0 })
+              : debit.toLocaleString('fr-FR', { style: 'currency', currency: 'EUR' })}
+          </Text>
+          <Text className="text-gray-600 text-base mt-1" style={{ color: Colors.labelSummary }}>Débit</Text>
+        </View>
+      </View>
+      <View className="items-center">
+        <View className='items-end'>
+          <Text className="text-lg font-semibold font-[Inter] text-green-400" style={{ color: Colors.green.text }}>
+            {balance % 1 === 0
+              ? balance.toLocaleString('fr-FR', { style: 'currency', currency: 'EUR', minimumFractionDigits: 0, maximumFractionDigits: 0 })
+              : balance.toLocaleString('fr-FR', { style: 'currency', currency: 'EUR' })}
+          </Text>
+          <Text className="text-gray-600 text-base mt-1" style={{ color: Colors.labelSummary }}>Solde</Text>
+        </View>
+      </View>
+    </View>
+  );
+} 

723292ac124cc0d68e1afdf9212e37a5f8f089e2
CharlyNd <charles.etienne.ndiaye@gmail.com>
2025-05-27 13:38:53 +0200

    style: add color-scheme

---
+++ constants/Colors.ts
@@ -0,0 +1,35 @@
+export default {
+  green: {
+    text: "#0ED094",
+    bg: "#E7FBF5"
+  },
+  red: {
+    text: "#FD755F",
+  },
+  blue: {
+    text: "#11ABEC",
+    bg: "#E7F6FF"
+  },
+  purple: {
+    text: "#F6F4FF",
+    bg: "#5843BE"
+  },
+  black: {
+    text: "#000",
+  },
+  yellow: {
+    text: "#FAA11C",
+    bg: "#FFF5BF"
+  },
+  gray: {
+    bg: "#F3F5F8",
+  },
+  button: {
+    primary: "#11ABEC",
+  },
+  labelOperation: {
+    date: "#013A51",
+    numberOperation: "#686E75"
+  },
+  labelSummary: "#354A61"
+};

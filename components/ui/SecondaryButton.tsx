f8c43e9eb7ae9a4d019062229a3f642f4627209e
CharlyNd <charles.etienne.ndiaye@gmail.com>
2025-05-28 15:38:46 +0200

    style: change secondaryButton to Pressable

--- components/ui/SecondaryButton.tsx
+++ components/ui/SecondaryButton.tsx
@@ -1,35 +1,25 @@
 import { Entypo } from '@expo/vector-icons';
 import { forwardRef } from 'react';
-import { Text, TouchableOpacity, TouchableOpacityProps, View } from 'react-native';
+import { Text, Pressable, PressableProps, View } from 'react-native';
 import Colors from '~/constants/Colors';
 
-interface SecondaryButtonProps extends TouchableOpacityProps {
+interface SecondaryButtonProps extends PressableProps {
   title: string;
 }
 
 export const SecondaryButton = forwardRef<View, SecondaryButtonProps>(
   ({ title, disabled, ...props }, ref) => {
     return (
-      <TouchableOpacity
+      <Pressable
         ref={ref}
-        activeOpacity={0.8}
         disabled={disabled}
         {...props}
+        className={`w-full rounded-2xl py-4 px-4 flex-row items-center ${disabled ? 'opacity-50' : ''} ${props.className || ''}`}
         style={{ backgroundColor: Colors.green.bg }}
-        className={`
-          w-full
-          rounded-2xl
-          py-4
-          px-4
-          flex-row
-          items-center
-          ${disabled ? 'opacity-50' : ''}
-          ${props.className || ''}
-        `}
       >
-        <Text className="font-semibold text-lg flex-1" style={{ color: Colors.green.textTag }}>{title}</Text>
+        <Text className="font-semibold text-base flex-1" style={{ color: Colors.green.textTag }}>{title}</Text>
         <Entypo name="chevron-thin-right" size={20} color={Colors.labelSummary} />
-      </TouchableOpacity>
+      </Pressable>
     );
   }
 );
